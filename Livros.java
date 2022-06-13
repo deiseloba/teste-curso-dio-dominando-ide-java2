83c42c6bb29b5d29a3605a9c6406125369268a67
deiseloba <106619466+deiseloba@users.noreply.github.com>
2022-06-13 20:26:48 -0300

    Add files via upload

---
+++ Livros.java
@@ -0,0 +1,15 @@
+
+public class Livros {
+
+	public static void main(String[] args) {
+
+	}
+               public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+			private String nome;
+               private String npag;
+}
