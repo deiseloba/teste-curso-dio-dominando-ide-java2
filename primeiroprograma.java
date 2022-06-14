1f3ddb608cd5ca8516473d8635eed63451796008
deiseloba <106619466+deiseloba@users.noreply.github.com>
2022-06-13 22:10:35 -0300

    Add files via upload

---
+++ primeiroprograma.java
@@ -0,0 +1,28 @@
+package com.dio;
+
+import br.com.dio.model.Gate;
+
+public class primeiroprograma {
+
+	     public static void main(String[] args) {
+		
+		    Gate gate= new Gate();
+		    Livros livros = new Livros();
+		      
+		    System.out.println(gate );
+		    System.out.println(livros);
+		    
+		    /*int a= 2;
+		        int b= 3;
+		         System.out.println("Hello World! "+ (2+3));*/
+
+	}
+
+}
+
+class Livros {
+	   private String nome;
+	   private String npag;
+}
+		
+	
